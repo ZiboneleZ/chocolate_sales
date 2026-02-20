0995c1e26f9c810d1881c7633ea43baaebb3389d
ZiboneleZ <zibonelez47@gmail.com>
2026-02-20 10:29:02 +0200

    Modified my cleaner into a function

--- chocolate_analyzer.py
+++ chocolate_analyzer.py
@@ -1,23 +1,29 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-#Loading data from the CSV file into a DataFrame
-df = pd.read_csv('ChocolateSales.csv')
+def data_cleaner(filepath='ChocolateSales.csv'):
 
-#Data before cleaning
-print('--- Data Before Being Cleaned ---\n')
-print(df.head())
+    #Loading data from the CSV file into a DataFrame
+    df = pd.read_csv(filepath)
 
-#Cleaning my data by removing the $ sign and converting to float
-df['Amount'] = df['Amount'].replace('[\$,]', '', regex=True).astype(float)
+    #Data before cleaning
+    print('--- Data Before Being Cleaned ---\n')
+    print(df.head())
 
-#Converting date into datetime objects
-df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
+    #Cleaning my data by removing the $ sign and converting to float
+    df['Amount'] = df['Amount'].replace('[\$,]', '', regex=True).astype(float)
 
-#Creating columns to use for analysis
-df['Price Per Box'] = df['Amount'] / df['Boxes Shipped']
-df['Year Month'] = df['Date'].dt.to_period('M')
+    #Converting date into datetime objects
+    df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
 
-print('\nData cleaning complete. Ready for analysis! Here is a sample below.\n')
-print(df.head())
+    #Creating columns to use for analysis
+    df['Price Per Box'] = df['Amount'] / df['Boxes Shipped']
+    df['Year Month'] = df['Date'].dt.to_period('M')
+
+    print('\nData cleaning complete. Ready for analysis! Here is a sample below.\n')
+
+    return df
+
+if __name__ == "__main__":
+    data = data_cleaner()
+    print('\nClean Data Preview:\n')
+    print(data.head())
